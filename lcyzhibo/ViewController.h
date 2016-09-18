00e06eff08d80752270304de92b1da1a130f428c
lincuiyuan112 <lincuiyuan8244@163.com>
2016-09-18 23:44:16 +0800

    init commit

---
+++ lcyzhibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lcyzhibo
+//
+//  Created by LCY on 16/9/18.
+//  Copyright © 2016年 lincuiyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lcyzhibo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  lcyzhibo
+//
+//  Created by LCY on 16/9/18.
+//  Copyright © 2016年 lincuiyuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
